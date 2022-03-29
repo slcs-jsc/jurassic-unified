bc5b4e6ee6c66200d2f13965115c8f81deaf95d2
Stjepan Pozgaj <stjepan.pozgaj1@gmail.com>
2022-03-29 21:36:54 +0200

    again add testing folder

---
+++ jurassic-scatter/testing/small_testset/jurun-ice-785.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -x
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=24
+#SBATCH --output=out
+#SBATCH --error=err
+#SBATCH --time=00:05:00
+#SBATCH --partition=gpus
+#SBATCH --gres=gpu:1
+#SBATCH --account=slen
+
+module load Python/3.8.5
+module load GCC/9.3.0
+module load OpenMPI/4.1.0rc1
+module load CUDA/11.3
+module load Nsight-Systems/2021.1.1
+module load Nsight-Compute/2020.3.0
+module load ParaStationMPI/5.4.7-1
+module load mpi-settings/CUDA
+module load GCCcore/.10.3.0
+module load GCCcore/.9.3.0
+module load GSL/2.6 
+
+export OMP_NUM_THREADS=12
+
+### set up
+src=$1
+w1=785
+w2=798
+
+### run forward model
+time srun $src/formod cloud-${w1}-${w2}.ctl obs33.tab atm.tab submissions/rad-${2}.tab AEROFILE aero.tab DIRLIST aux/first_001
---
+++ jurassic-scatter/testing/small_testset/submit.sh
@@ -0,0 +1,25 @@
+make clean -C ../../jurassic-gpu/src/
+make libutil.a -C ../../jurassic-gpu/src/
+
+sleep 2
+make clean -C ../../src
+make -C ../../src
+
+rm out
+n=$(< aux/submission_index)
+m=$(( n + 1 ))
+echo $m > aux/submission_index
+echo last: $n, new: $m
+ls ../../jurassic-gpu/src/ -rtl | tail -1
+ls ../../src/ -rtl | tail -1
+
+read -n 1 -s -r -p "Press any key to continue"
+echo
+sbatch jurun-ice-785.sh ../../src $m
+
+watch -n 1 squeue -u pozgaj1
+
+python3 diff.py
+read -n 1 -s -r -p "Press any key to continue"
+echo
+less out
