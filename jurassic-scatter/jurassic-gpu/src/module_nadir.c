65bef0240e63352b2da6520b8502b443d362b67a
Stjepan Pozgaj <stjepan.pozgaj1@gmail.com>
2022-06-02 22:55:07 +0200

    add module_nadir.c

---
+++ jurassic-scatter/jurassic-gpu/src/module_nadir.c
@@ -0,0 +1,36 @@
+#include "jurassic.h"
+#include "sca_forwardmodel.h"
+
+int main(int argc, char *argv[]) {
+  
+  static ctl_t ctl;
+  static obs_t obs;
+  
+  double dlat, lat, latmax, latmin, obsz;
+  
+  /* Check arguments... */
+  if(argc<7)
+    ERRMSG("Give parameters: <ctl> <obsz> <latmin> <latmax> <dlat> <obs>");
+  
+  /* Read forward model control parameters... */
+  jur_read_ctl(argc, argv, &ctl);
+  
+  /* Read arguments... */
+  obsz=atof(argv[2]);
+  latmin=atof(argv[3]);
+  latmax=atof(argv[4]);
+  dlat=atof(argv[5]);
+  
+  /* Create measurement geometry... */
+  for(lat=latmin; lat<=latmax; lat+=dlat) {
+    obs.obsz[obs.nr]=obsz;
+    obs.vplat[obs.nr]=lat;
+    if((++obs.nr)>=NRMAX)
+      ERRMSG("Too many rays!");
+  }
+  
+  /* Write observation data... */
+  jur_write_obs(NULL, argv[6], &ctl, &obs);
+  
+  return EXIT_SUCCESS;
+}
